002bb78af35b5d08a6a05b57f082b292ee86ab19
Alexander Ulitin <ulitiy@gmail.com>
2020-12-22 00:43:40 +0500

    repeat animation

--- DoodleMaster/Web/task.js
+++ DoodleMaster/Web/task.js
@@ -23,8 +23,10 @@ function loadSVG(text) {
 }
 
 let stepNumber;
+let refreshVersion = 0;
 
 function hideAll() {
+  refreshVersion++;
   const el = document.querySelector(`.show`);
   if (!el) return;
   el.classList.remove("show");
@@ -45,46 +47,63 @@ function showInput(step) {
   stepNumber = step;
   document.querySelector(`.step-${step} .input`).classList.add("show");
   document.querySelectorAll(`.step-${step} .input .draw-line`).forEach((el) => drawLine(el));
-  document.querySelectorAll(`.step-${step} .input .draw-point`).forEach(drawPoint);
   if (debugSVG) return;
   window.requestAnimationFrame(() =>
     window.requestAnimationFrame(() =>
       window.webkit.messageHandlers.control.postMessage('InputReady')));
 }
 
-function drawPoint(el) {
-  let dashLen = 50;
-  el.style.transition = "";
-  // remove delay, it breaks animation
-  tempRemoveDelay(el)
+function getRepeat(el) {
+  for (var cl of el.classList.values()) {
+    const match = cl.match(/^r(\d+)$/);
+    if (match) {
+      return parseInt(match[1]);
+    }
+  }
+  return undefined;
+}
 
-  let len = el.getTotalLength() * 1.1;
-  let seconds = Math.round(len/100)/10;
-  el.style.strokeDasharray = `${dashLen} ${len}`;
-  el.style.strokeDashoffset = len + dashLen * 2;
-
-  setTimeout(() => {
-    let flip = el.classList.contains("flip");
-    el.style.transition = `stroke-dashoffset ${seconds}s linear`;
-    el.style.strokeDashoffset = (flip ? len + dashLen : dashLen);
-  }, 0);
+function getDelay(el) {
+  for (var cl of el.classList.values()) {
+    const match = cl.match(/^d(\d+)$/);
+    if (match) {
+      return parseInt(match[1]);
+    }
+  }
+  return undefined;
 }
 
 function drawLine(el) {
+  const tempVersion = refreshVersion;
+  const repeatDelay = getRepeat(el);
+  const delay = getDelay(el); // don't move this line
   el.style.transition = "";
   // remove delay, it breaks animation
-  tempRemoveDelay(el)
+  tempRemoveDelay(el);
 
   let len = el.getTotalLength() * 1.1;
-  let seconds = Math.round(len/100)/10;
+  let duration = Math.round(len/100)/10;
   el.style.strokeDasharray = len;
   el.style.strokeDashoffset = len;
+  el.classList.remove("hide");
 
   setTimeout(() => {
     let flip = el.classList.contains("flip");
-    el.style.transition = `stroke-dashoffset ${seconds}s linear`;
+    el.style.transition = `stroke-dashoffset ${duration}s linear, opacity 0.3s linear`;
     el.style.strokeDashoffset = flip ? len * 2 : 0;
   }, 0);
+
+  if (repeatDelay == undefined) return;
+
+  console.log(delay, duration, repeatDelay);
+  setTimeout(() =>{
+    if (tempVersion != refreshVersion) return;
+    el.classList.add("hide");
+  }, duration * 1000); // there's additional delay for hide
+  setTimeout(() => {
+    if (tempVersion != refreshVersion) return;
+    drawLine(el);
+  }, (delay + duration + 0.3 + repeatDelay) * 1000);
 }
 
 function tempRemoveDelay(el) {

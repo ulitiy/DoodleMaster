7f87ad5513456f8105d3a9023a8fce2b417b950f
Alexander Ulitin <ulitiy@gmail.com>
2021-01-20 16:13:19 +0500

    CourseListView

--- DoodleMaster/Views/CourseListView.swift
+++ DoodleMaster/Views/CourseListView.swift
@@ -10,12 +10,38 @@ import SwiftUI
 
 struct CourseListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 300, maximum: 400), spacing: 80)],
+                      alignment: .center, spacing: 80) {
+                ForEach(0...20, id: \.self) { i in
+                    NavigationLink(destination: LazyView(TaskView(task: courses[0].tasks[0]))) {
+                        VStack {
+                            CarouselView {
+                                Image("1-1").resizable().aspectRatio(contentMode: .fill)
+                                Image("1-2").resizable().aspectRatio(contentMode: .fill)
+                                Image("1-3").resizable().aspectRatio(contentMode: .fill)
+                                Image("1-4").resizable().aspectRatio(contentMode: .fill)
+                            }.aspectRatio(1,contentMode: .fit)
+                            Text("Basic human body proportions")
+                                .font(.custom("LucidaGrande", size: 25))
+                                .foregroundColor(Color(hex: "303b96ff"))
+                                .multilineTextAlignment(.center)
+                                .padding(.top, 10)
+                        }
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }.padding(50)
+        }
+        .navigationBarTitle("")
+        .navigationBarHidden(true)
     }
 }
 
 struct CourseListView_Previews: PreviewProvider {
     static var previews: some View {
         CourseListView()
+            .previewDevice("iPad (8th generation)")
+            .previewLayout(.fixed(width: 1366, height: 1024))
     }
 }
--- DoodleMaster/Views/RootView.swift
+++ DoodleMaster/Views/RootView.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct RootView: View {
     var body: some View {
         NavigationView {
-            TaskListView()
+            CourseListView()
         }
         .navigationViewStyle(StackNavigationViewStyle())
         .statusBar(hidden: true)

84dc5f4fb28308aa5c3ea4f514f71a3d1a45817c
Alexander Ulitin <ulitiy@gmail.com>
2021-01-20 16:12:44 +0500

    fix CarouselView bugs and improve usability

--- DoodleMaster/Views/Components/CarouselView.swift
+++ DoodleMaster/Views/Components/CarouselView.swift
@@ -7,54 +7,71 @@
 //
 
 import SwiftUI
+import Combine
 
+//https://stackoverflow.com/questions/58896661/swiftui-create-image-slider-with-dots-as-indicators
 struct CarouselView<Content>: View where Content: View {
-//    @Binding var index: Int
     let maxIndex: Int
-    let content: () -> Content
+    var content: () -> Content
 
     @State private var offset = CGFloat.zero
     @State private var dragging = false
     @State var index = 0
+    @State var cancelDrag: AnyCancellable?
 
-    init(maxIndex: Int, @ViewBuilder content: @escaping () -> Content) {
-        //index: Binding<Int>,
-//        self._index = index
-        self.maxIndex = maxIndex
+    init(@ViewBuilder content: @escaping () -> Content) {
+        let v = Mirror(reflecting: content()).descendant("value")
+        let tm = Mirror(reflecting: v!)
+        self.maxIndex = tm.children.count - 1
         self.content = content
+    }
+    
+    func onEnded(_ value: _ChangedGesture<DragGesture>.Value, geometry: GeometryProxy) {
+        cancelDrag?.cancel()
+        let predictedEndOffset = -CGFloat(self.index) * geometry.size.width + value.predictedEndTranslation.width
+        let predictedIndex = Int(round(predictedEndOffset / -(geometry.size.width+0.0001)))
+        self.index = self.clampedIndex(from: predictedIndex)
+        print(index)
+        withAnimation(.easeOut) { // animation only on false!
+            self.dragging = false
+        }
     }
 
     var body: some View {
         ZStack(alignment: .bottom) {
             GeometryReader { geometry in
-                ScrollView(.horizontal, showsIndicators: false) {
+                Group {
                     HStack(spacing: 0) {
-                        self.content()
+                        content()
                             .frame(width: geometry.size.width, height: geometry.size.height)
                             .clipped()
-                    }
+                            .offset(x: self.offset(in: geometry), y: 0)
+                    }.allowsHitTesting(false)
                 }
-                .content.animation(.easeOut).offset(x: self.offset(in: geometry), y: 0)
-                .frame(width: geometry.size.width, alignment: .leading)
-                .gesture(
-                    DragGesture().onChanged { value in
-                        self.dragging = true
-                        self.offset = -CGFloat(self.index) * geometry.size.width + value.translation.width
-                    }
-                    .onEnded { value in
-                        let predictedEndOffset = -CGFloat(self.index) * geometry.size.width + value.predictedEndTranslation.width
-                        let predictedIndex = Int(round(predictedEndOffset / -geometry.size.width))
-                        self.index = self.clampedIndex(from: predictedIndex)
-                        withAnimation(.easeOut) { // animation only on false!
-                            self.dragging = false
-                        }
-                    }
+                .frame(width: geometry.size.width, height: geometry.size.height, alignment: .leading)
+                .clipped()
+                .overlay(
+                    Rectangle()
+                        .opacity(0.00001)
+                        .gesture(
+                            DragGesture(minimumDistance: 20).onChanged { value in
+                                self.dragging = true
+                                self.offset = -CGFloat(self.index) * geometry.size.width + max(min(value.translation.width, geometry.size.width), -geometry.size.width)
+                                let just = Just<Bool>(true).delay(for: .seconds(1), scheduler: RunLoop.main)
+                                cancelDrag?.cancel()
+                                cancelDrag = just.sink { _ in
+                                    onEnded(value, geometry: geometry)
+                                }
+                            }
+                            .onEnded { value in
+                                onEnded(value, geometry: geometry)
+                            }
+                            , including: .gesture)
                 )
             }
-            .clipped()
-
             PageIndicator(index: $index, maxIndex: maxIndex)
         }
+        .overlay(RoundedRectangle(cornerRadius: 40).stroke(Color(hex: "303b96ff")!, lineWidth: 4))
     }
 
     func offset(in geometry: GeometryProxy) -> CGFloat {
@@ -78,9 +95,15 @@ struct PageIndicator: View {
     var body: some View {
         HStack(spacing: 8) {
             ForEach(0...maxIndex, id: \.self) { index in
-                Circle()
-                    .fill(index == self.index ? Color(hex: "ccccccff")! : Color.gray)
-                    .frame(width: 8, height: 8)
+                if index == self.index {
+                    Circle()
+                        .fill(index == self.index ? Color(hex: "303b96ff")! : Color(hex: "bbbbbbff")!)
+                        .frame(width: 9.2, height: 9.2)
+                } else {
+                    Circle()
+                        .stroke(Color(hex: "303b96ff")!, lineWidth: 1.2)
+                        .frame(width: 8, height: 8)
+                }
             }
         }
         .padding(15)
@@ -89,11 +112,21 @@ struct PageIndicator: View {
 
 struct CarouselView_Previews: PreviewProvider {
     static var previews: some View {
-        CarouselView(maxIndex: 3) {
-            MyImageView(name: "1-1")
-            MyImageView(name: "1-2")
-            MyImageView(name: "1-3")
-            MyImageView(name: "1-4")
+        HStack(spacing: 100) {
+            CarouselView {
+                MyImageView(name: "1-1")
+                MyImageView(name: "1-2")
+                MyImageView(name: "1-3")
+            }.frame(width: 200, height: 200).padding(.top, 100)
+            CarouselView {
+                MyImageView(name: "1-1")
+                MyImageView(name: "1-2")
+                MyImageView(name: "1-3")
+                MyImageView(name: "1-4")
+            }.frame(width: 200, height: 200)
         }
+        .previewDevice("iPad (8th generation)")
+        .previewLayout(.fixed(width: 1366, height: 1024))
+
     }
 }
